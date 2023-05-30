a5c3f39c454c9f954bb50e18b219d1c0c9892388
@rharison <rharison.abreu@gmail.com>
2023-05-30 13:18:01 -0300

    feat: criando middleware para validar id e verificar se existe o carro

---
+++ back-end/src/api/middlewares/car.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express"
+import { validateId } from '@validation/id'
+import CarService from '@services/car'
+
+const isValidId = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params
+
+    validateId(id)
+
+    next()
+  } catch (error: any) {
+    const { code, fields, message } = error
+
+    return res.status(code).json({
+      message,
+      fields
+    })
+  }
+}
+
+export const hasCar = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params
+
+    await CarService.get(id)
+
+    next()
+  } catch (error: any) {
+    const { code, fields, message } = error
+
+    return res.status(code).json({
+      message,
+      fields
+    })
+  }
+}
+
+export default {
+  isValidId,
+  hasCar
+}
