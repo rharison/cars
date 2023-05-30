0d6a37d784089b69ce1002d72cc14fb28c03011e
@rharison <rharison.abreu@gmail.com>
2023-05-30 17:10:51 -0300

    feat: atualizando teste de validations

--- back-end/src/tests/validations.test.ts
+++ back-end/src/tests/validations.test.ts
@@ -1,9 +1,11 @@
 import assert from 'assert';
 import { v4 as uuidV4 } from 'uuid'
 import {
+  validateDataCar,
   validate,
   messageErrorInvalidByField,
-  MINIMUM_YEAR
+  MINIMUM_YEAR,
+  MESSAGE_ERROR_REQUIRED_FIELD
 } from '@validation/car';
 import {
   validateId,
@@ -47,6 +49,114 @@ describe('Validations individual fields', () => {
     const result = validate('ano', validAno);
 
     assert.strictEqual(result, false);
+  });
+
+  it('Must return false when data car is valid in create and update operation', () => {
+    const validDataCar = {
+      placa: 'aaa-1234',
+      chassi: '123456789',
+      modelo: 'Uno',
+      marca: 'Fiat',
+      ano: 2022
+    }
+
+    const resultCreateOperation = validateDataCar(validDataCar, 'create');
+    const resultUpdateOperation = validateDataCar(validDataCar, 'update');
+
+    assert.strictEqual(resultCreateOperation, false);
+    assert.strictEqual(resultUpdateOperation, false);
+  });
+
+  it('Should return correct error message when data car is invalid in create and update operation', () => {
+    const invalidDataCar = {
+      placa: 'aaa-1234',
+      chassi: '123456789',
+      modelo: 'Uno',
+      marca: 'Fiat()',
+      ano: 2022
+    }
+
+    assert.throws(() => validateDataCar(invalidDataCar, 'create'), {
+      name: 'InvalidsFieldsError',
+      message: 'These fields are invalids',
+      fields: [
+        { field: 'marca', message: messageErrorInvalidByField['marca'] }
+      ]
+    });
+
+    assert.throws(() => validateDataCar(invalidDataCar, 'update'), {
+      name: 'InvalidsFieldsError',
+      message: 'These fields are invalids',
+      fields: [
+        { field: 'marca', message: messageErrorInvalidByField['marca'] }
+      ]
+    });
+  });
+
+  it('Should return correct error message when car data is sent without any mandatory data in the creation operation', () => {
+    const baseDataCar = {
+      placa: 'aaa-1234',
+      chassi: '123456789',
+      modelo: 'Uno',
+      marca: 'Fiat',
+      ano: 2022
+    }
+    const returnName = 'RequiredFieldsError'
+
+    const dataCarWithouRequiredFieldPlaca = {
+      ...baseDataCar,
+      placa: undefined
+    } as any
+
+    const dataCarWithouRequiredFieldChassi = {
+      ...baseDataCar,
+      chassi: undefined
+    } as any
+
+    const dataCarWithouRequiredFieldModelo = {
+      ...baseDataCar,
+      modelo: undefined
+    } as any
+
+    const dataCarWithouRequiredFieldMarca = {
+      ...baseDataCar,
+      marca: undefined
+    } as any
+
+    const dataCarWithouRequiredFieldAno = {
+      ...baseDataCar,
+      ano: undefined
+    } as any
+
+    assert.throws(() => validateDataCar(dataCarWithouRequiredFieldPlaca, 'create'), {
+      name: returnName,
+      message: MESSAGE_ERROR_REQUIRED_FIELD,
+      fields: ['placa']
+    });
+
+    assert.throws(() => validateDataCar(dataCarWithouRequiredFieldChassi, 'create'), {
+      name: returnName,
+      message: MESSAGE_ERROR_REQUIRED_FIELD,
+      fields: ['chassi']
+    });
+
+    assert.throws(() => validateDataCar(dataCarWithouRequiredFieldModelo, 'create'), {
+      name: returnName,
+      message: MESSAGE_ERROR_REQUIRED_FIELD,
+      fields: ['modelo']
+    });
+
+    assert.throws(() => validateDataCar(dataCarWithouRequiredFieldMarca, 'create'), {
+      name: returnName,
+      message: MESSAGE_ERROR_REQUIRED_FIELD,
+      fields: ['marca']
+    });
+
+    assert.throws(() => validateDataCar(dataCarWithouRequiredFieldAno, 'create'), {
+      name: returnName,
+      message: MESSAGE_ERROR_REQUIRED_FIELD,
+      fields: ['ano']
+    });
   });
 
   it('Should return correct error message when field placa is invalid', () => {
