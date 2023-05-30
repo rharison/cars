244730deab444ad13694059a7e0771f876414296
@rharison <rharison.abreu@gmail.com>
2023-05-30 16:08:07 -0300

    feat: adicionando porta padrão caso não seja definida no .env

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -4,14 +4,13 @@ import { env } from '@/utils/env'
 import { carRoutes }  from '@routes/car'
 
 const app = express()
-const PORT = env('PORT')
+const PORT = env('PORT') || 3000
 
 app.disable('x-powered-by').disable('etag')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(carRoutes)
-
 
 export function start() {
   return new Promise(resolve => {
